3c6933d619c06e9eef40840521383234a9245846
ShimaMomen <shimasadat.momen@outlook.de>
2025-12-22 14:27:13 +0100

    editing the test inventory count

--- automatisierte-tests/tests/e2e/inventorypage.spec.ts
+++ automatisierte-tests/tests/e2e/inventorypage.spec.ts
@@ -1,26 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test('Product page', async ({ page }) => {
+test('Inventory page', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.getByRole('textbox', { name: 'Username'}).fill('standard_user');
   await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
   await page.getByRole('button', { name: 'Login' }).click();
   
-// Verify that we are on the products page
+// Verify that we are on the inventory page
   const titleLocator = page.locator('[data-test="title"]');
   await expect(titleLocator).toHaveText('Products');
   
-  // Verify that there are 6 products displayed on the page
+  // Verify that there are 6 products displayed on this page
   const items = page.locator('.inventory_item');
   await expect(items).toHaveCount(6);
 
 // Verify that each product has an "Add to cart" button
-  const AddToCartButtons = page.locator('.inventory_item').getByRole('button', { name: 'Add to cart' });
+  const addToCartButtons = page.getByRole('button', { name: 'Add to cart' });
+  await expect(addToCartButtons).toHaveCount(6);
 
-  for (let i = 0; i < 6;  i++) {
-    const button = AddToCartButtons.nth(i);
-    await expect(button).toHaveText('Add to cart');  
-  }
 
   
 
